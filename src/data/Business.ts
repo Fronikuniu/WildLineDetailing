0f94a4686f9b9f44fbd551ea53c8116b9d982bc2
Fronikuniu <Fronikuniu@gmail.com>
2023-10-05 23:54:58 +0200

    Change email

--- src/data/Business.ts
+++ src/data/Business.ts
@@ -3,5 +3,5 @@ export const business = {
   address: 'Sikorskiego 443, 35-304 Rzeszów',
   addressLink: 'https://goo.gl/maps/BE8k99gorwzKS5vp9',
   phone: '+48690844443',
-  email: 'wildlinedetailing@gmail.pl',
+  email: 'wildlinedetailing@gmail.com',
 };
