bb12710892ef655182cb61b0c82e95a5bc85d36a
Fronikuniu <Fronikuniu@gmail.com>
2023-11-03 21:43:32 +0100

    Change move key to env file

--- src/getData/getGallery.tsx
+++ src/getData/getGallery.tsx
@@ -2,11 +2,8 @@ import React, { useEffect, useState } from 'react';
 import Gallery from '@/components/Gallery';
 import { IImage } from '@/components/types';
 
-const KEY =
-  'IGQWRPc084RHVKamJGc2hMZAXNoOFNydjRMT2JvVVNLR05tR0gwT2h0LS00aUxSQWk3UEtEcTBqZA00tN1lTaEhxY0s3ZA1hXU0FlSzNSd0VjYmt1TGZAUWUFyZAEFYNV93cEQ0cE15YmRkeFNfWDdEYno1ZAHZABRWpoMDgZD';
-
 const getGallery = async () => {
-  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${KEY}`;
+  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalink&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_KEY}`;
   const data = await fetch(url);
   const feed = await data.json();
   return feed.data as IImage[];
