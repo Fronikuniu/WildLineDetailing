1a6f7dbe32656f90724e888a22d086598e2b4b59
Fronikuniu <Fronikuniu@gmail.com>
2024-01-30 20:46:18 +0100

    Change meta description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import Footer from '@/components/Footer';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
-  title: 'Wildline Detailing | Myjnia Detailing Samochodów Rzeszów',
+  title: 'Wildline Detailing - Myjnia, Detailing Samochodów Rzeszów',
   description:
-    'Zanurz swoje auto w luksusowej pielęgnacji w naszej myjni i auto detailingu. Oferujemy kompleksowe usługi mycia, polerowania i zabezpieczania lakieru, dbając o każdy detal Twojego pojazdu. Sprawdź, jak możemy przywrócić blask i ochronić Twój samochód. Zaufaj ekspertom w dziedzinie detailingu samochodowego!',
+    'Oferujemy kompleksowe usługi mycia, polerowania i zabezpieczania lakieru, dbając o każdy detal Twojego pojazdu. Sprawdź, jak możemy przywrócić blask i ochronić Twój samochód. Zaufaj ekspertom w dziedzinie detailingu samochodowego!',
 };
 
 export default function RootLayout({
